9db51a4c04feb978eb73ac93275df51b519ff450
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-17 04:20:54 +0530

    format with prettier

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
 interface IHeadProps {
     title?: string;
