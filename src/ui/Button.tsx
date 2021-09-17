5ccbf85983beeb87dadad50de77a35d174956dbc
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-18 03:43:31 +0530

    add transition to button colors

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -16,7 +16,8 @@ export function Button({
 }: ButtonProps) {
     const Icon = icon || null;
     let buttonClasses =
-        'flex items-center justify-center bg-primary hover:bg-onPrimaryHover active:bg-primary py-3 px-6 font-bold rounded-xl';
+        'flex items-center justify-center bg-primary hover:bg-onPrimaryHover ' +
+        'active:bg-primary py-3 px-6 font-bold rounded-xl transition-colors duration-200 ease-out';
 
     if (variant === 'secondary') {
         buttonClasses +=
