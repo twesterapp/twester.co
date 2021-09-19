be63919cf0c3405eba254719e7b0c11ff21ebad3
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-20 00:41:12 +0530

    v0.1.5 download links

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const APP_VERSION = '0.1.3';
+export const APP_VERSION = '0.1.5';
 export const GITHUB_LINK = 'https://github.com/twesterapp/twester';
 export const WINDOWS_LINK =
-    'https://github.com/twesterapp/releases/releases/download/v0.1.3/Twester-Setup-0.1.3.exe';
+    'https://github.com/twesterapp/releases/releases/download/v0.1.5/Twester-Setup-0.1.5.exe';
 export const LINUX_LINK =
-    'https://github.com/twesterapp/releases/releases/download/v0.1.3/Twester-0.1.3.AppImage';
+    'https://github.com/twesterapp/releases/releases/download/v0.1.5/Twester-0.1.5.AppImage';
 export const MACOS_LINK =
-    'https://github.com/twesterapp/releases/releases/download/v0.1.3/Twester-0.1.3.dmg';
+    'https://github.com/twesterapp/releases/releases/download/v0.1.5/Twester-0.1.5.dmg';
