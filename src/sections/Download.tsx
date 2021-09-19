5fa0859a6fedf736dc89a5196fce07c5f759f235
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-19 12:34:47 +0530

    change open source info in downloads section

--- src/sections/Download.tsx
+++ src/sections/Download.tsx
@@ -9,8 +9,8 @@ export function Download() {
                     <h2 className="text-center text-alter">Download</h2>
                     <p className="text-center text-alter mt-2">
                         Twester is developed out in the open on our GitHub page.
-                        You can join the discussion, request for features,
-                        report issues or just check the source code there!
+                        You can report issues, request features, ask questions
+                        or just check the source code there!
                     </p>
                     <div className="flex flex-col sm:justify-center sm:flex-row space-y-3 sm:space-y-0 sm:space-x-2 md:space-x-8 mt-6 sm:mt-9">
                         <Button icon={IconWindows} variant="secondary">
