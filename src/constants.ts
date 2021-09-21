a24c8818d8f30b7cc6ad4f90f027dce04c8cbcd3
Shikhar Sharma <ceoshikhar@gmail.com>
2021-09-21 14:36:38 +0530

    remove version from constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-// TODO: Get rid of the version and link stuff. Use functions from `github.ts`
-// after releasing v1.0.0
-export const APP_VERSION = '0.1.5';
 export const GITHUB_REPO_LINK = 'https://github.com/twesterapp/twester';
 
 export const GITHUB_ORG_NAME = 'twesterapp';
